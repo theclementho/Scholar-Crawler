77757b01367d21e29e2cfec25f616ee3f212eb10
Chris Huh <chris.huh@mail.utoronto.ca>
2020-03-16 11:30:55 -0400

    Database connection setup

---
+++ database/setup/todo_list.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Integer
+from base import Base
+
+
+class TodoList(Base):
+    __tablename__ = 'todo_list'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    type = Column(String)
+    status = Column(Integer)
+
+    def __init__(self, name, type, status):
+        self.name = name
+        self.type = type
+        self.status = status
+
+    def __repr__(self):
+        return "<TodoList(name='{0}', type='{1}', status='{2}'" \
+            .format(self.name, self.type, self.status)
+            
