e818a0a76e1ea711448dee37e6ac09077dfeccba
Chris Huh <chris.huh@mail.utoronto.ca>
2020-03-16 19:01:49 -0400

    added storage side apis

--- database/api.py
+++ database/api.py
@@ -64,12 +64,8 @@ def add_authors(authors):
 # start = time.time()
 # add_author("Different Chris", "akdfjjas", "University of Toronto", "Self isolation, social distancing")
 # end = time.time()
-# print('Elapsed time: ', end - start)
 
 
-# time.sleep(0.5)
-
-# test get profile
 # test = get_profile("dklfjalejE")
 # print(test)
 
