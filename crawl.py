0a9965a45c7569b35dd228af346fe04ba431af0e
Clement <theclementho@users.noreply.github.com>
2020-03-17 15:25:22 -0400

    Added scraped data into local files

--- crawl.py
+++ crawl.py
@@ -3,6 +3,8 @@ import sys
 import requests
 import csv, json
 import time
+import traceback
+import logging
 from datetime import datetime
 scholarlypath = "scholarly-master/scholarly/"
 sys.path.append(scholarlypath)
@@ -75,7 +77,7 @@ def retrieveAuthorPage(profileID):
         print(str(e))
 
 # Create a hash set containing everyone that has worked with the author, and what paper was their most recent one.
-# Hash set structure: {name: [year, paper_title]}
+# Hash set structure: {[name, year, paper_title]}
 # Also take their coauthors and add it to the end of our to-search queue
 def generateCoAuthorDict(profile):
     """Generates a dictionary that contains all of the author's coauthors, the year that they last worked together, and the title of the paper they wrote, given an AuthorProfile object."""
@@ -101,7 +103,7 @@ def generateCoAuthorDict(profile):
             for name in set_paper_author:
                 return_authors.append([name, year, title])
                 set_authors.add(name)
-        print("Returning data structure: {0}".format(return_authors))
+        # print("Returning data structure: {0}".format(return_authors))
         return return_authors
     except Exception as e:
         logf = openLogging()
@@ -112,22 +114,123 @@ def generateCoAuthorDict(profile):
 def writeToDatabase(profile):
     return
 
+def saveNewAuthor(authProfile):
+    with open("logs/AUTH_{0}.txt".format(authProfile.id), 'w') as saveFile:
+        author_info = dict()
+        author_info[authProfile.id] = {
+            'name': authProfile.name,
+            'gs_profile_id': authProfile.id,
+            'affiliation': authProfile.affiliation,
+            'interest': authProfile.interests,
+        }
+        json.dump(author_info, saveFile)
+    return
+
+def saveNewRelations(id, relationLists):
+    with open("logs/RELATIONS_{0}.txt".format(id), 'w') as saveFile:
+        authRelations = dict()
+        authRelations[id] = relationLists
+        json.dump(authRelations, saveFile)
+    return
+
+def saveSets(visited_labels, unvisited_labels, visited_orgs, unvisited_orgs, visited_authors, unvisited_authors):
+    with open("logs/visited_labels.csv", 'w') as saveFile:
+        wr = csv.writer(saveFile)
+        while (len(visited_labels) != 0):
+            item = visited_labels.pop()
+            wr.writerow([item])
+    with open("logs/unvisited_labels.csv", 'w') as saveFile:
+        wr = csv.writer(saveFile)
+        while (len(unvisited_labels) != 0):
+            item = unvisited_labels.pop()
+            wr.writerow([item])
+    with open("logs/visited_orgs.csv", 'w') as saveFile:
+        wr = csv.writer(saveFile)
+        while (len(visited_orgs) != 0):
+            item = visited_orgs.pop()
+            wr.writerow([item])
+    with open("logs/unvisited_orgs.csv", 'w') as saveFile:
+        wr = csv.writer(saveFile)
+        while (len(unvisited_orgs) != 0):
+            item = unvisited_orgs.pop()
+            wr.writerow([item])
+    with open("logs/visited_authors.csv", 'w') as saveFile:
+        wr = csv.writer(saveFile)
+        while (len(visited_authors) != 0):
+            item = visited_authors.pop()
+            wr.writerow([item])
+    with open("logs/unvisited_authors.csv", 'w') as saveFile:
+        wr = csv.writer(saveFile)
+        while (len(unvisited_authors) != 0):
+            item = unvisited_authors.pop()
+            wr.writerow([item])
+    return
+
+def readSets():
+    try:
+        labels = set()
+        orgs = set()
+        authors = set()
+        unvisited_labels = set()
+        unvisited_orgs = set()
+        unvisited_authors = set()
+        with open("logs/visited_labels.csv") as readFile:
+            lis = [line.split() for line in readFile]
+            for x in lis:
+                labels.add(x[0])
+        with open("logs/visited_orgs.csv") as readFile:
+            lis = [line.split() for line in readFile]
+            for x in lis:
+                orgs.add(x[0])
+        with open("logs/visited_authors.csv") as readFile:
+            lis = [line.split() for line in readFile]
+            for x in lis:
+                authors.add(x[0])
+        with open("logs/unvisited_labels.csv") as readFile:
+            lis = [line.split() for line in readFile]
+            for x in lis:
+                unvisited_labels.add(x[0])
+        with open("logs/unvisited_orgs.csv") as readFile:
+            lis = [line.split() for line in readFile]
+            for x in lis:
+                unvisited_orgs.add(x[0])
+        with open("logs/unvisited_authors.csv") as readFile:
+            lis = [line.split() for line in readFile]
+            for x in lis:
+                unvisited_authors.add(x[0])
+    except Exception as e:
+        print(str(e))
+    finally:
+        return labels, orgs, authors, unvisited_labels, unvisited_orgs, unvisited_authors
+
 # TODO: Multithread this method
-def scrapeListContent(generator):
+def scrapeListContent(generator, visited_authors):
     """Given a generator function, scrape all authors and return a dictionary of information to send to server"""
     # new_relations: {authID: {'coAuth Name': [year, 'Paper name'], ...}}
     new_profiles = set()
-    new_relations = dict()
     label_set = set()
     organization_set = set()
     for profile_array in generator:
+        if (profile_array[0] in visited_authors):
+            print("Already done ID", profile_array[0])
+            continue
+        # Try multiple times, sometimes it returns none
+        # auth_profile = None
+        # for i in range(5):
         auth_profile = retrieveAuthorPage(profile_array[0])
-        new_profiles.add(auth_profile)
+            # if auth_profile is not None:
+                # break
+        if auth_profile is None:
+            print("Cannot find ID",profile_array[0])
+            continue
+        saveNewAuthor(auth_profile)
+        new_profiles.add(auth_profile.id)
 
         # TODO: Add the coauthors written on their page to a queue "to search"
 
         coauthor_array = generateCoAuthorDict(auth_profile)
-        new_relations[auth_profile.id] = coauthor_array
+        saveNewRelations(auth_profile.id, coauthor_array)
+        # new_relations[auth_profile.id] = coauthor_array
 
         # Add the labels and organizations into their sets
         label_set = label_set | profile_array[2]
@@ -135,9 +238,9 @@ def scrapeListContent(generator):
             organization_set.add(auth_profile.organization)
 
     ## After testing, remove label_set and organization_set from return
-    return new_profiles, new_relations, label_set, organization_set
+    return new_profiles, label_set, organization_set
 
-def manageSearchSets():
+def manageSearchSets(first_label):
     """This method spins up new threads to search each label and organization number.
     Start with an organization, and then labels, etc"""
 
@@ -146,9 +249,9 @@ def manageSearchSets():
     visited_labels = set() # Label [string]
     unvisited_labels = set() # Label [string]
     visited_orgs = set() # IDs [string]
-    unvisited_ orgs = set() # IDs [string]
+    unvisited_orgs = set() # IDs [string]
     visited_authors = set() # IDs [string]
-    related_coauthors = set() # IDs [string]
+    unvisited_authors = set() # IDs [string]
     # related_coauthors stores the coauthors that are present in the sidebar of the profile currently being scanned; this will build a secondary list
 
     # Step 1: Take one label and scrape all of the profiles they produce
@@ -156,19 +259,51 @@ def manageSearchSets():
 
     # Step 2: After searching the labels, we have to search all of the related_coauthors that are on the sidebars of those places
     
-    # Step 3: Continue searching the labels until the list is empty, and then try searching all of the organizations, building up labels 
+    # Step 3: Continue searching the labels until the list is empty, and then try searching all of the organizations, building up labels
 
-    return
+    visited_labels, visited_orgs, visited_authors, unvisited_labels, unvisited_orgs, unvisited_authors= readSets()
+    print("Skipping authors:",visited_authors)
+    # print("Skipping labels:",visited_labels)
+    # print("Skipping orgs:",visited_orgs)
+
+    try:
+        generator = None
+        while (True):
+            if (len(unvisited_labels) != 0):
+                # Take a random label off the list, doesn't matter which
+                search = unvisited_labels.pop()
+                unvisited_labels.discard(search)
+                visited_labels.add(search)
+                generator = retrieveAuthorsFromLabels(search)
+            else:
+                if (len(unvisited_orgs) != 0):
+                    # Take a random org number off the list, doesn't matter which
+                    search = unvisited_orgs.pop()
+                    unvisited_orgs.discard(search)
+                    visited_orgs.add(search)
+                    generator = retrieveAuthorsFromOrgNum(search)
+                else:
+                    return
+
+            if (generator != None):
+                scrapedProfiles, label_set, org_set = scrapeListContent(generator, visited_authors)
+                visited_authors = visited_authors | scrapedProfiles
+                new_labels = label_set - visited_labels
+                new_orgs = org_set - visited_orgs
+                unvisited_labels = unvisited_labels | new_labels
+                unvisited_orgs = unvisited_orgs | new_orgs
+            else:
+                print("Error! generator returned None! Skipping search query", search)
+                break
+    except Exception as e:
+        logging.error(traceback.format_exc())
+    finally:
+        saveSets(visited_labels, unvisited_labels, visited_orgs, unvisited_orgs, visited_authors, unvisited_authors)
 
 def main():
     # Loop over the whole thing until both are empty
     # while(label_queue or organization_queue):
     """STEP 1: Get the author profiles from every profile in the organization"""
-    # Replace placeholder_orgnum with search_query = organization_queue.popleft()
-    # Each element in auth_list contains [ID, name, interests]
-    auth_list = retrieveAuthorsFromOrgNum(placeholder_orgnum)
-
-    scrapeListContent(auth_list)
 
     # TODO: Create set of parameters for author profile and coauthor information to send to database
 
@@ -176,8 +311,7 @@ def main():
 
     # TODO: append the final organization_set and label_set to their respective queues
 
-    """STEP 2: Take a label and crawl authors related to those"""
-    # Replace placeholder_label with search_query = label_queue.popleft()
-    auth_list = retrieveAuthorsFromLabels(placeholder_label)
+    manageSearchSets("Debugging")
 
-    scrapeListContent(auth_list)
+if __name__ == "__main__":
+    main()
