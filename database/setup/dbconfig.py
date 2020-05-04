360e587461fce02c49950dce4ea016d1442fd1a4
Chris Huh <chris.huh@mail.utoronto.ca>
2020-05-04 18:15:46 -0400

    dbconfig file not ignored anymore

---
+++ database/setup/dbconfig.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+local = {
+  'host': 'localhost',
+  'username': 'LOCAL_USERNAME',
+  'password': 'LOCAL_PASSWORD',
+  'db': 'LOCAL_DBNAME'
+}
+
+rds = {
+  'host': 'RDS_HOST',
+  'username': 'RDS_USERNAME',
+  'password': 'RDS_PASSWORD',
+  'db': 'RDS_DBNAME'
+}
+  
